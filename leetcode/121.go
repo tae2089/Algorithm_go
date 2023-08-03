8bf77082ce6422729ecaa56a93757a4ea439b441
tae2089 <tae2089002@gmail.com>
2023-08-03 18:35:01 +0900

    leetcode 121 maxProfit

---
+++ leetcode/121.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func maxProfit(prices []int) int {
+	minPrice := prices[0]
+	maxProfit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		}
+		if prices[i]-minPrice > maxProfit {
+			maxProfit = prices[i] - minPrice
+		}
+	}
+	return maxProfit
+}
