ac44cede5447c3ba114e607c3a3d1708c9ba6f1c
tae2089 <tae2089002@gmail.com>
2023-08-08 21:53:54 +0900

    test: :test_tube: leetcode 125. Valid Palindrome

---
+++ leetcode/125_test.go
@@ -0,0 +1,44 @@
+package leetcode
+
+import "testing"
+
+func Test_isPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				s: "A man, a plan, a canal: Panama",
+			},
+			want: true,
+		},
+		{
+			name: "",
+			args: args{
+				s: "race a car",
+			},
+			want: false,
+		},
+		{
+			name: "",
+			args: args{
+				s: "0P",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("isPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
